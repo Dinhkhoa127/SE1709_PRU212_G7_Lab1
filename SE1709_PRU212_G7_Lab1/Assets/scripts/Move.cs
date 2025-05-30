14201cea5fa2c3776ca09a8c613b7f05c2793588
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-05-30 22:37:07 +0700

    fix space outside quoc 30/5/2025

--- SE1709_PRU212_G7_Lab1/Assets/scripts/Move.cs
+++ SE1709_PRU212_G7_Lab1/Assets/scripts/Move.cs
@@ -2,24 +2,37 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
     private Vector2 movement;
+
+    [SerializeField] private float minX = -8.47f;
+    [SerializeField] private float maxX = 8.47f;
+    [SerializeField] private float minY = -4.47f;
+    [SerializeField] private float maxY = 4.47f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();
     }
-     void FixedUpdate()
+
+    void FixedUpdate()
     {
-        rb.linearVelocity = movement * GameManager.instance.GetPlayerSpeed();
+        Vector2 newPos = rb.position + movement * GameManager.instance.GetPlayerSpeed() * Time.fixedDeltaTime;
+        newPos = ClampPosition(newPos);
+        rb.MovePosition(newPos);
+    }
+
+    Vector2 ClampPosition(Vector2 pos)
+    {
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        return pos;
     }
 }
